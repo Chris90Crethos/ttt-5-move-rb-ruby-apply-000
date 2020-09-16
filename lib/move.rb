bf13999d04dfba4586373df3d1dda0c3a7f6c6f7
Chris Martin <bmg.martin08@gmail.com>
2020-09-16 15:30:18 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,13 +8,10 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(user_input)
-  user_input.to_i -1
+  user_input.to_i
 end
 
 
 def move (index, token)
   @board[index] = token
 end
-
-
- 
